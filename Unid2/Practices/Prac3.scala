fd6fc7e6d39ae561ca158b9f9ea712c33c310d3a
israelpablo <77422159+israelpablo@users.noreply.github.com>
2022-05-06 07:35:31 -0700

    updata pract2 and prac3

--- Unid2/Practices/Prac3.scala
+++ Unid2/Practices/Prac3.scala
@@ -1,44 +1,43 @@
-package org.apache.spark.examples.ml;
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.DecisionTreeClassificationModel
-import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
-import spark.implicits._
-import org.apache.spark.ml.feature.Normalizer
-// Load the data stored in LIBSVM format as a DataFrame.
-val data = spark.read.format("libsvm").load("c:/Spark/data/mllib/sample_libsvm_data.txt")
+
+// Load and parse the data file, converting it to a DataFrame.
+val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
 // Index labels, adding metadata to the label column.
 // Fit on whole dataset to include all labels in index.
 val labelIndexer = new StringIndexer()
-  labelIndexer.setInputCol("label")
-  labelIndexer.setOutputCol("indexedLabel")
-  labelIndexer.fit(data)
+  .setInputCol("label")
+  .setOutputCol("indexedLabel")
+  .fit(data)
 // Automatically identify categorical features, and index them.
+// Set maxCategories so features with > 4 distinct values are treated as continuous.
 val featureIndexer = new VectorIndexer()
-  featureIndexer.setInputCol("features")
-  featureIndexer.setOutputCol("indexedFeatures")
-  featureIndexer.setMaxCategories(4) // features with > 4 distinct values are treated as continuous.
-  featureIndexer.fit(data)
+  .setInputCol("features")
+  .setOutputCol("indexedFeatures")
+  .setMaxCategories(4)
+  .fit(data)
 
 // Split the data into training and test sets (30% held out for testing).
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
-// Train a DecisionTree model.
-val dt = new DecisionTreeClassifier()
-  dt.setLabelCol("indexedLabel")
-  dt.setFeaturesCol("indexedFeatures")
+// Train a RandomForest model.
+val rf = new RandomForestClassifier()
+  .setLabelCol("indexedLabel")
+  .setFeaturesCol("indexedFeatures")
+  .setNumTrees(10)
 
 // Convert indexed labels back to original labels.
 val labelConverter = new IndexToString()
-  labelConverter.setInputCol("prediction")
-  labelConverter.setOutputCol("predictedLabel")
-  labelConverter.setLabels(labelIndexer.labels)
+  .setInputCol("prediction")
+  .setOutputCol("predictedLabel")
+  .setLabels(labelIndexer.labelsArray(0))
 
-// Chain indexers and tree in a Pipeline.
+// Chain indexers and forest in a Pipeline.
 val pipeline = new Pipeline()
-  .setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+  .setStages(Array(labelIndexer, featureIndexer, rf, labelConverter))
 
 // Train model. This also runs the indexers.
 val model = pipeline.fit(trainingData)
@@ -57,5 +56,5 @@ val evaluator = new MulticlassClassificationEvaluator()
 val accuracy = evaluator.evaluate(predictions)
 println(s"Test Error = ${(1.0 - accuracy)}")
 
-val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
-println(s"Learned classification tree model:\n ${treeModel.toDebugString}")
+val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
